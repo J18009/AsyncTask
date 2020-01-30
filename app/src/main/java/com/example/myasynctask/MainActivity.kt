5d52b35f1eb6a666ac48ebec6fc11b12f92c99ee
J18009 <j18009@sangi.jp>
2020-01-30 09:56:02 +0900

    commit

---
+++ app/src/main/java/com/example/myasynctask/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myasynctask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(),View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+            if (v.id == R.id.button) {
+                MyAsyncTask(this).execute("Param1")
+        }
+    }
+}
